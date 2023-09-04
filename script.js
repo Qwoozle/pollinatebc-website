a8c91619b9b51a7daac0b1e59a4833f4b3075882
Timothy Mai <88265897+Tim7513@users.noreply.github.com>
2023-09-04 04:34:27 -0700

    Update script.js
    Signed-off-by: Timothy Mai <88265897+Tim7513@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-<script>
-        const initiativesLink = document.getElementById("initiatives");
-        const leadersLink = document.getElementById("leaders");
 
-        const initiativesContent = document.getElementById("initiativesContent");
-        const leadersContent = document.getElementById("leadersContent");
-
-        initiativesLink.addEventListener("click", () => {
-            initiativesContent.style.display = "block";
-            leadersContent.style.display = "none";
-        });
-
-        leadersLink.addEventListener("click", () => {
-            initiativesContent.style.display = "none";
-            leadersContent.style.display = "block";
-        });
-    </script>
